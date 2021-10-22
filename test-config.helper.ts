4e11cfbca6b7dd9e3426426d0d3e115fc4224116
baptiste.gho <baptiste.gho@epita.fr>
2021-10-22 10:45:21 +0200

    fixed tests

--- test-config.helper.ts
+++ test-config.helper.ts
@@ -1,10 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { PianoComponent } from 'src/app/piano/piano.component';
 
 type CompilerOptions = Partial<{
   providers: any[];
   useJit: boolean;
   preserveWhitespaces: boolean;
 }>;
+
+beforeEach(() => {
+  TestBed.configureTestingModule({
+    declarations: [
+      PianoComponent       
+    ],
+  });
+  return TestBed.compileComponents();
+});
+
 export type ConfigureFn = (testBed: typeof TestBed) => void;
 
 export const configureTests = (configure: ConfigureFn, compilerOptions: CompilerOptions = {}) => {
