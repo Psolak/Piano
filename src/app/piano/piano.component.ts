93537ce334c23a7e35503ca4acae869c743b86b1
baptiste.gho <baptiste.gho@epita.fr>
2021-10-22 11:35:05 +0200

    Added guess note feature and fixed bugs

--- src/app/piano/piano.component.ts
+++ src/app/piano/piano.component.ts
@@ -10,14 +10,14 @@ export class PianoComponent implements OnInit {
 
 
   colors:string[];
-  lastplayed:number;
+  guessme:number;
   notes: string[];
 
   constructor() {
 
     this.colors = ["", "", "", "", "", "", "", "", "", "", "", ""];
     this.notes = ["C", "Csharp", "D", "Dsharp", "E", "F", "Fsharp", "G", "Gsharp", "A", "Asharp", "B"]
-    this.lastplayed = 0;
+    this.guessme = -1;
    }
 
 
@@ -25,50 +25,85 @@ export class PianoComponent implements OnInit {
     
   }
 
-  public clickKey(n:number)
+  public clickKey(n:number, show:boolean)
   {
-    this.colors[this.lastplayed] = "";
-    this.colors[n] = "colored"
-    this.lastplayed = n
+
+    if (this.guessme >= 0)
+    {
+
+      let oldColor = this.colors[n];
+      if (n==this.guessme)
+      {
+      this.colors[n] = "right"
+      }
+      else
+      {
+        this.colors[n] = "wrong"
+      }
+      setTimeout(() =>{
+        this.colors[n] = oldColor;
+      }, 500)
+      this.guessme = -1;
+    }
+    else if (show)
+    {
+      let oldColor = this.colors[n];
+      this.colors[n] = "colored"
+      setTimeout(() =>{
+        this.colors[n] = oldColor;
+      }, 500)
+    }
+      
+  
     let audio = new Audio();
     audio.src = "assets/"+ this.notes[n] +".wav"
     audio.load()
     audio.play()
   }
+
   public guessNote()
   {
-    this.colors[this.lastplayed] = "";
+    this.guessme = -1;
     let piano = new PianoComponent() 
     
     let index = Math.floor(Math.random() * (10))
-    this.colors[index] = "colored"
-    this.clickKey(index)
-    this.lastplayed = index;
+    this.clickKey(index, false)
+    this.guessme = index;
+  }
+
+  private resetColors()
+  {
+    for (var _i = 0; _i < this.colors.length; _i++)
+    {
+       this.colors[_i] = "";
+    }
   }
 
   public chooseScale(scale:string)
   {
+    this.resetColors();
     switch(scale)
     {
-      case "C" : {
-        this.colors[0] = "colored"
-        this.colors[2] = "colored"
-        this.colors[4] = "colored"
-        this.colors[5] = "colored"
-        this.colors[7] = "colored"
-        this.colors[9] = "colored"
-        this.colors[11] = "colored"
+      case "C" :
+      case "Am" :
+        {
+        this.colors[0] = "scale"
+        this.colors[2] = "scale"
+        this.colors[4] = "scale"
+        this.colors[5] = "scale"
+        this.colors[7] = "scale"
+        this.colors[9] = "scale"
+        this.colors[11] = "scale"
         break;
       }
-
       case "Cm" : {
-        this.colors[0] = "colored"
-        this.colors[2] = "colored"
-        this.colors[3] = "colored"
-        this.colors[5] = "colored"
-        this.colors[7] = "colored"
-        this.colors[8] = "colored"
-        this.colors[10] = "colored"
+        this.colors[0] = "scale"
+        this.colors[2] = "scale"
+        this.colors[3] = "scale"
+        this.colors[5] = "scale"
+        this.colors[7] = "scale"
+        this.colors[8] = "scale"
+        this.colors[10] = "scale"
         break;
       }
     }
